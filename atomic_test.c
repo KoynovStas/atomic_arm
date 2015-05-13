7f160eb866ebc0cb9446a8b11454495856b89157
Koinov Stas <koinov@sinkross.ru>
2015-05-13 14:20:34 +0300

    atomic_test.c - add tests for atomic_fetch_or function

--- atomic_test.c
+++ atomic_test.c
@@ -398,6 +398,144 @@ int test_atomic_and_6(struct test_info_t  *test_info)
 
 
 
+
+
+// -------------------  OR  --------------------
+
+int test_atomic_or_1(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = 0;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 1);
+
+    if( (i != 1) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_or_2(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = 2;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 2);
+
+    if( (i != 2) ||  (ret != 2) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_or_3(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = 0xFFFF;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 0xFE);
+
+    if( (i != 0xFFFF) ||  (ret != 0xFFFF) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_or_4(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = -1;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 0);
+
+    if( (i != -1) ||  (ret != -1) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_or_5(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = 0;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 0xFF00);
+
+    if( (i != 0xFF00) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    ret = atomic_fetch_or(&i, 0x00F0);
+
+    if( (i != 0xFFF0) ||  (ret != 0xFF00) )
+        return TEST_BROKEN;
+
+
+
+    ret = atomic_fetch_or(&i, 0x000F);
+
+    if( (i != 0xFFFF) ||  (ret != 0xFFF0) )
+        return TEST_BROKEN;
+
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_or_6(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    unsigned int i = 0xFFFFAA55;
+    unsigned int ret;
+
+
+    ret = atomic_fetch_or(&i, 0xFFFF5A5A);
+
+    if( (i != 0xFFFFFA5F) ||  (ret != 0xFFFFAA55) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
 ptest_func tests[] =
 {
 
@@ -426,6 +564,15 @@ ptest_func tests[] =
     test_atomic_and_4,
     test_atomic_and_5,
     test_atomic_and_6,
+
+
+    //or
+    test_atomic_or_1,
+    test_atomic_or_2,
+    test_atomic_or_3,
+    test_atomic_or_4,
+    test_atomic_or_5,
+    test_atomic_or_6,
 };
 
 
