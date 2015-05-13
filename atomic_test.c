1c950dae4e3405cda63d93429c8e0ebdbf4e8698
Koinov Stas <koinov@sinkross.ru>
2015-05-13 14:16:19 +0300

    atomic_test.c - add tests for atomic_fetch_add function

--- atomic_test.c
+++ atomic_test.c
@@ -36,10 +36,133 @@ typedef  int (*ptest_func)(struct test_info_t  *test_info);
 
 
 
+// --------------   ADD  ------------------------
+
+int test_atomic_add_1(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = 0;
+    int ret;
+
+
+    ret = atomic_fetch_add(&i, 1);
+
+    if( (i != 1)  ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_add_2(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = 0;
+    int ret;
+
+    ret = atomic_fetch_add(&i, 2);
+
+    if( (i != 2) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_add_3(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = 0;
+    int ret;
+
+    ret = atomic_fetch_add(&i, -2);
+
+    if( (i != -2) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_add_4(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = -2;
+    int ret;
+
+    ret = atomic_fetch_add(&i, 2);
+
+    if( (i != 0) ||  (ret != -2) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_add_5(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = 0;
+    int ret;
+
+    ret = atomic_fetch_add(&i, 0xFFFFFF);
+
+    if( (i != 0xFFFFFF) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
+
+int test_atomic_add_6(struct test_info_t  *test_info)
+{
+
+    TEST_INIT;
+
+    int i = 0;
+    int ret;
+
+    ret = atomic_fetch_add(&i, 0);
+
+    if( (i != 0) ||  (ret != 0) )
+        return TEST_BROKEN;
+
+
+    return TEST_PASSED;
+}
+
+
 
 ptest_func tests[] =
 {
 
+    //add
+    test_atomic_add_1,
+    test_atomic_add_2,
+    test_atomic_add_3,
+    test_atomic_add_4,
+    test_atomic_add_5,
+    test_atomic_add_6,
 
 };
 
